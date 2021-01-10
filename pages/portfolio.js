1f9814523019dc4d8760028abb221c78f218dcfc
StevenDRiggs <sfriggs21@gmail.com>
2021-01-09 23:16:41 -0700

    set up grid layout for portfolio

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,7 +1,34 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+import CIL from '../images/CIL.svg'
+
+import styles from '../styles/Portfolio.module.css'
+
+
 const Portfolio = () => {
   return (
-    <div>
-      Portfolio
+    <div className={styles.portfolioGrid}>
+      <Link href='#'>
+        <a>
+          <CIL className={styles.thumbnail} />
+        </a>
+      </Link>
+      <Link href='#'>
+        <a>
+          <CIL className={styles.thumbnail} />
+        </a>
+      </Link>
+      <Link href='#'>
+        <a>
+          <CIL className={styles.thumbnail} />
+        </a>
+      </Link>
+      <Link href='#'>
+        <a>
+          <CIL className={styles.thumbnail} />
+        </a>
+      </Link>
     </div>
   )
 }
