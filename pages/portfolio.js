f060704de438dd4b51b5dfdbd88bd83e39195637
StevenDRiggs <sfriggs21@gmail.com>
2021-01-14 20:07:38 -0700

    finished yeti svg

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -8,41 +8,41 @@ import styles from '../styles/Portfolio.module.css'
 
 const Portfolio = () => {
   return (
-    <div className={styles.svgTester}>
-      <Yeti className={styles.testItem} />
-    </div>
-    // <div className={styles.portfolioGrid}>
-    //   <Link href='#'>
-    //     <a>
-    //       <CIL className={styles.thumbnail} />
-    //       <caption className={styles.caption}>Calapitter Image Library</caption>
-    //     </a>
-    //   </Link>
-    //   <Link href='#'>
-    //     <a>
-    //       <CIL className={styles.thumbnail} />
-    //       <caption className={styles.caption}>Yeti The Bookclub App</caption>
-    //     </a>
-    //   </Link>
-    //   <Link href='#'>
-    //     <a>
-    //       <CIL className={styles.thumbnail} />
-    //       <caption className={styles.caption}>Recipe Calculator</caption>
-    //     </a>
-    //   </Link>
-    //   <Link href='#'>
-    //     <a>
-    //       <CIL className={styles.thumbnail} />
-    //       <caption className={styles.caption}>Ruled.me Web Scraper CLI</caption>
-    //     </a>
-    //   </Link>
-    //   <Link href='#'>
-    //     <a>
-    //       <CIL className={styles.thumbnail} />
-    //       <caption className={styles.caption}>Seed, Time and Harvest Database</caption>
-    //     </a>
-    //   </Link>
+    // <div className={styles.svgTester}>
+    //   <Yeti className={styles.testItem} />
     // </div>
+    <div className={styles.portfolioGrid}>
+      <Link href='#'>
+        <a>
+          <CIL className={styles.thumbnail} />
+          <caption className={styles.caption}>Calapitter Image Library</caption>
+        </a>
+      </Link>
+      <Link href='#'>
+        <a>
+          <Yeti className={styles.thumbnail} />
+          <caption className={styles.caption}>Yeti The Bookclub App</caption>
+        </a>
+      </Link>
+      <Link href='#'>
+        <a>
+          <CIL className={styles.thumbnail} />
+          <caption className={styles.caption}>Recipe Calculator</caption>
+        </a>
+      </Link>
+      <Link href='#'>
+        <a>
+          <CIL className={styles.thumbnail} />
+          <caption className={styles.caption}>Ruled.me Web Scraper CLI</caption>
+        </a>
+      </Link>
+      <Link href='#'>
+        <a>
+          <CIL className={styles.thumbnail} />
+          <caption className={styles.caption}>Seed, Time and Harvest Database</caption>
+        </a>
+      </Link>
+    </div>
   )
 }
 
