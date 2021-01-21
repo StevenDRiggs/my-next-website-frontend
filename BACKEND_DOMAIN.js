734ba84416ed10a9523bca3e60f8731c255294c4
StevenDRiggs <sfriggs21@gmail.com>
2021-01-20 20:29:10 -0700

    updated database url

--- BACKEND_DOMAIN.js
+++ BACKEND_DOMAIN.js
@@ -1,4 +1,5 @@
-const BACKEND_DOMAIN = 'http://localhost:8080'
+// const BACKEND_DOMAIN = 'http://localhost:8080'
+const BACKEND_DOMAIN = 'https://still-lake-47635.herokuapp.com'
 
 
 export default BACKEND_DOMAIN
