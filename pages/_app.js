f3cf5f4b4a330a4e2fdfc9b93a722fbef1aa107f
StevenDRiggs <sfriggs21@gmail.com>
2021-03-07 21:16:34 -0700

    switch to blog for reference

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+import { storeWrapper } from '../redux/store/store'
+import Navbar from '../components/navbar'
+import Logo from '../images/logoReact.svg'
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>Steven Riggs | {Component.name ? Component.name : Component.WrappedComponent.name}</title>
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Fugaz+One&display=swap' />
+      </Head>
+
+      <Navbar />
+
+      <Link href='/'>
+        <a>
+          <Logo className='chrysalis-logo' />
+        </a>
+      </Link>
+
+      <div className='main-display'>
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
 
-export default MyApp
+
+export default storeWrapper.withRedux(App)
