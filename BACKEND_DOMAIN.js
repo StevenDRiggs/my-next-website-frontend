287fb5ba08ee51deda8a1921554bea8c12b08e4f
StevenDRiggs <sfriggs21@gmail.com>
2021-03-09 17:51:12 -0700

    updated BACKEND_DOMAIN.js

--- BACKEND_DOMAIN.js
+++ BACKEND_DOMAIN.js
@@ -1,5 +1,5 @@
-const BACKEND_DOMAIN = 'http://localhost:8080'
-// const BACKEND_DOMAIN = 'https://stevendriggs-backend.herokuapp.com'
+// const BACKEND_DOMAIN = 'http://localhost:8080'
+const BACKEND_DOMAIN = 'https://stevendriggs-backend.herokuapp.com'
 
 
 export default BACKEND_DOMAIN
